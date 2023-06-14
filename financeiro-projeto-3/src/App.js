14bf2aea5e78f9621202b9010a9506b0e198eb0f
Andre Poli <andre.lpoli@hotmail.com>
2023-06-13 21:08:39 -0300

    react criado

---
+++ financeiro-projeto-3/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={<div>financeiro-projeto-3</div>} />
+      </Routes>
+    </div>
+  );
+};
+export default App;
